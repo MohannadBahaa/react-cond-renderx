5f64a846a9063fd8f14caea20f688078cf1fb501
Omar Dulaimi <o.m.dulaimi@gmail.com>
2020-08-08 01:13:20 +0300

    current state

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+import React from "react";
+type PredicateType = () => boolean;
+type ComponentsType = Array<{ comp: any; props: any }>;
+type ParamsType = { predicate: PredicateType; components: ComponentsType };
+
+function rcrx(params: ParamsType) {
+  if (params.predicate()) {
+    return React.cloneElement(
+      params.components[0].comp,
+      params.components[0].props,
+      null
+    );
+  } else {
+    return React.cloneElement(
+      params.components[1].comp,
+      params.components[1].props,
+      null
+    );
+  }
+}
